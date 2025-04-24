a92c2bb72a57271bb9dcd7535bfee23db3660bae
Elad-27 <156566730+Elad-27@users.noreply.github.com>
2025-04-24 15:34:39 +0300

    Update terminal.c

--- LegOS/terminal.c
+++ LegOS/terminal.c
@@ -1,5 +1,5 @@
-/* LegOS Copyright(C) 2025 Elad Yaakobi
-This program is distributed in the hope that it will be useful,
+/* 		LegOS Copyright(C) 2025 Elad Yaakobi
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
