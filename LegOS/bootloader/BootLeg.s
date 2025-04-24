381bed89382d00ef7537b1978220b998dfed0abb
Elad-27 <156566730+Elad-27@users.noreply.github.com>
2025-04-24 15:36:24 +0300

    Update BootLeg.s

--- LegOS/bootloader/BootLeg.s
+++ LegOS/bootloader/BootLeg.s
@@ -1,3 +1,14 @@
+/* 		LegOS Copyright(C) 2025 Elad Yaakobi
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 /* Declare constants for the multiboot header. */
 .set ALIGN,    1<<0             /* align loaded modules on page boundaries */
 .set MEMINFO,  1<<1             /* provide memory map */
